a2392a4e67f9e3f744a2fcefa9ea23249d7e8404
Devmunds <devmunds@gmail.com>
2020-07-09 22:55:11 -0300

    Add files default

---
+++ fretestore.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin name:         FreteStore
+ * Plugin uri:              https://github.com/Devmunds/FreteStore
+ * Description:           Plugin para calculo de frete com api Devmunds
+ * Version:                 1.0.0
+ * Requires at least:   4.1.0
+ * WC tested up to:    4.1.0
+ * Author:                   Devmunds
+ * Author uri:              https://devmunds.com.br/
+ * License:                  GPLv2 or later
+ * License uri:             https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ **/
